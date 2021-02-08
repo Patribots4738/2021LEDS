6d4422da1821fe7f442759a18267bb6f5d8559aa
Wynter-Tau <33078460+Wynter-Tau@users.noreply.github.com>
2021-02-08 14:45:05 -0800

    fix

--- src/Test.h
+++ src/Test.h
@@ -1,6 +1,5 @@
 #include<Arduino.h>
 #include<FastLED.h>
-#include<PrecisionDraw.h>
 
 extern CRGB j_leds [];
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,10 @@
 
 CRGB j_leds[NUM_LEDS] ={0};
 
-#include "Marquee.h"
-#include "Twinkle.h"
+//#include "Marquee.h"
+//#include "Twinkle.h"
 //#include "Comet.h"
-#include "Bounce.h"
+//#include "Bounce.h"
 #include "Test.h"
 
 #define ARRAYSIZE(x) (sizeof(x)/sizeof(x[0]))
@@ -45,4 +45,43 @@ void loop() {
   
   FastLED.delay(15);
 
+}
+
+
+CRGB ColorFraction(CRGB colorIn, float fraction) {
+
+  fraction = min(1.0f, fraction);
+  return CRGB(colorIn).fadeToBlackBy(225 * (1.0f - fraction));
+
+}
+
+void DrawPixels(float fPos, float count, CRGB color) {
+
+  // Calclulate how much the first pixel will hold
+  float availFirstPixel = 1.0f - (fPos - (long)(fPos));
+  float amtFirstPixel = min(availFirstPixel, count);
+  float remaining = min(count, FastLED.size() - fPos);
+  int iPos = fPos;
+
+  // Blend in the color of the first partial pixel
+
+  if (remaining > 0.0f)
+  {
+    FastLED.leds() [iPos++] += ColorFraction(color, amtFirstPixel);
+    remaining -= amtFirstPixel;
+  }
+
+  // Now draw pixels in the middle
+  while (remaining > 1.0f)
+  {
+    FastLED.leds() [iPos++] += color;
+    remaining--;
+  }
+  
+  // Draw tail pixel, up to a single pixel
+  if (remaining > 0.0f)
+  {
+    FastLED.leds() [iPos] += ColorFraction(color, remaining);
+  }
+  
 }
